6523c1e4d9f2d2ded1d9895d96a3757258ddb175
Mostafa-eskander <mosgadeskander@gmail.com>
2026-02-17 15:05:13 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import TodoList from './components/TodoList.jsx'
+
+const App = () => {
+  return (
+    <div className='grid place-items-center py-4 min-h-screen bg-violet-600'>
+      <TodoList />
+    </div>
+  )
+}
+
+export default App
+
